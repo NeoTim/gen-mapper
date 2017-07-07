99cb9d907fa19687e439e6ccad5cbf8e8daf73d4
Joel Cox <joelcox@Joels-MacBook-Pro-2.local>
2017-07-06 23:17:19 -0500

    add client login and signup pages

---
+++ scripts/auth/auth.js
@@ -0,0 +1,82 @@
+
+class UserAuthentication {
+    constructor() {
+        this.isNewUser = false;
+        this.confirmedPassword = null;
+        this.data = { email: null, password: null }
+    }
+
+    attached() {
+        this.container = document.querySelector('container');
+        this.form = document.querySelector('form');
+
+        this.showSignupButton = document.querySelector('#show-signup-toggle');
+        this.showLoginButton = document.querySelector('#show-login-toggle');
+        this.bind();
+    }
+
+    bind() {
+        let onkeyup = (event)=> {
+            this.toggleLoginButton(this.checkValidity());
+        }
+
+        let onsubmit = (event) => {
+            event.preventDefault();
+
+            if (this.checkValidity()) {
+                this.login();
+            }
+        }
+        
+        this.form.addEventListener('keyup', onkeyup);
+        this.form.addEventListener('submit', onsubmit);
+        
+        this.showSignupButton.addEventListener('click', (event)=> {
+            event.preventDefault();
+            this.container.classList.add('is-signup');
+            this.container.classList.remove('is-login');
+        })
+        
+        this.showLoginButton.addEventListener('click', (event)=> {
+            event.preventDefault();
+            this.container.classList.add('is-login');
+            this.container.classList.remove('is-signup');
+        })
+    }
+
+    toggleLoginButton(visible) {
+        this.form.classList[visible ? 'add' : 'remove']('show-login-button');
+    }
+
+    toggleCreateNewUser(bool) {
+        this.isNewUser = !this.isNewUser;
+        this.form.classList[bool ? 'add' : 'remove']('show-confirm-pass');
+    }
+    
+    checkValidity() {
+        if ( this.form.email.value && this.form.password.value ) {
+            if (this.isNewUser && this.form.password.value !== this.form.password2) {
+                return false;
+            }
+            return true
+        }
+        return false
+    }
+
+    login() {
+
+        const data = {
+            email: this.form.email.value,
+            password: this.form.password.value
+        }
+
+        $.ajax({
+            url: 'users/login',
+            method: 'POST',
+            data: data,
+            success: (data)=> {
+                console.log(data)
+            }
+        })
+    }
+}
